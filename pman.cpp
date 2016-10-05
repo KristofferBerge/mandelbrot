eed762006cf76d455a0e246aff61d207d157b5cd
Kristoffer Møgster Berge <Kristoffer.berge@gmail.com>
2016-10-05 22:02:46 +0200

    tweaking zoom

--- pman.cpp
+++ pman.cpp
@@ -8,11 +8,11 @@
 
 void master(int window_width,int window_height, int partitions, MPI_Datatype pixeltype){
 	GraphicsContainer gCont = getNewDisplay(window_width,window_height);
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD,&world_size);
 	MPI_Status status;
-	
-	for(int i = 0; i < world_size-2; i++){
+	long WHITE = WhitePixel(gCont.display, gCont.screen);
+	long BLACK = BlackPixel(gCont.display, gCont.screen);
+	long COLOR = 0;
+	for(int i = 0; i < partitions; i++){
 		MPI_Probe(MPI_ANY_SOURCE,12,MPI_COMM_WORLD,&status);
 		int sectionSize;
 		MPI_Get_count(&status, MPI_INT,&sectionSize);
@@ -23,16 +23,21 @@ void master(int window_width,int window_height, int partitions, MPI_Datatype pix
 		std::cout << "  TO: " << pixels[sectionSize].y << "\n";
 		
 		for(int j = 0; j < sectionSize; j++){
-			XSetForeground(gCont.display,gCont.gc,pixels[j].distance);
+			if(pixels[j].distance == 500)
+				COLOR = BLACK;
+			else
+				COLOR = WHITE-(500-pixels[j].distance)*(WHITE-BLACK)/500;
+			
+			XSetForeground(gCont.display,gCont.gc,COLOR);
 			XDrawPoint(gCont.display,gCont.window,gCont.gc,pixels[j].x,pixels[j].y);
 				
 			//std::cout << "x:"<< pixels[j].x << " y:" << pixels[j].y << " = " << pixels[j].distance;
 			//std::cout << "\n";
 		}
 		XFlush(gCont.display);
-		sleep(2);
 	}
 	//MPI_Send(NULL,0,MPI_INT,1,0,MPI_COMM_WORLD);	
+	sleep(4);
 	//MPI_Finalize();
 }
 
@@ -44,8 +49,10 @@ void delegator(int height, int width, int partitions){
 	int SECTION_TAG = 11;
 	MPI_Status status;
 	int cursor = 0;
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD,&world_size);
 	int step = height/partitions;
-	for(int i=0; i<partitions; i++){
+	for(int i=0; i<partitions + world_size -2; i++){
 		int node_id;
 		//Recieving request for new section
 		MPI_Recv(&node_id,1,MPI_INT,MPI_ANY_SOURCE,REQUEST_TAG,MPI_COMM_WORLD,&status);
@@ -54,6 +61,11 @@ void delegator(int height, int width, int partitions){
 		section[0] = 0; //Start X
 		section[1] = cursor; //Start Y
 		section[2] = width; //Stop X
+		
+		if(cursor >= height){
+			MPI_Send(section,4,MPI_INT,node_id,SECTION_TAG,MPI_COMM_WORLD);
+			continue;
+		}
 		if(height < cursor+step){
 			section[3] = height;
 		}
@@ -75,6 +87,7 @@ void slave(double x, double y, double s, MPI_Datatype pixeltype){
 	int SECTION_TAG = 11;
 	int DRAW_TAG = 12;
 	int rank;
+	int done = 0;
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Status status;
 
@@ -84,12 +97,16 @@ void slave(double x, double y, double s, MPI_Datatype pixeltype){
 //		pixels[i].y = 0;
 //		pixels[i].distance = 0;
 //	}
+	while(done == 0){
 	int section[4];
 	MPI_Send(&rank,1,MPI_INT,1,REQUEST_TAG,MPI_COMM_WORLD);
 	MPI_Recv(&section,4,MPI_INT,MPI_ANY_SOURCE,SECTION_TAG, MPI_COMM_WORLD,&status);
 	int sectionSize = (section[0]-section[2])*(section[1]-section[3]);
 	MandelbrotPixel pixels[sectionSize];
-
+	if(sectionSize == 0){
+		done = 1;
+		continue;
+	}
 	int arrayCursor = 0;
 	for(int i=section[0]; i<section[2]; i++){
 		for(int j=section[1]; j<section[3];j++){
@@ -103,6 +120,7 @@ void slave(double x, double y, double s, MPI_Datatype pixeltype){
 
 
 	MPI_Send(pixels,sectionSize,pixeltype, 0,DRAW_TAG,MPI_COMM_WORLD);
+	}
 //	for(int i = 0; i < 10; i++){
 //		getDistance(i,i,result);
 //		std::cout << result[i].distance << "\n";
@@ -134,15 +152,16 @@ int main(){
 	MPI_Type_commit(&pixeltype);
 	
 	//Size of window
-	int window_height,window_width;
+	int window_height,window_width,partitions;
 	//Size of fraction
 	double midX,midY,step;
 	//Setting initial zoom of fraction and standard window size
-	window_width = 600;
-	window_height = 300;
-	midX = -1;
-	midY = 0.5;
-	step = 0.01;
+	window_width = 1824;
+	window_height = 984;
+	midX = -0.088;
+	midY = 0.654;
+	step = 0.00001;
+	partitions = 50;
 		
 	int world_size,rank;
 	MPI_Comm_size(MPI_COMM_WORLD,&world_size);
@@ -157,11 +176,11 @@ int main(){
 	
 	if(rank == 0){
 		//Node is master
-		master(window_width, window_height,1,pixeltype);
+		master(window_width, window_height,partitions,pixeltype);
 	}
 	else if(rank == 1){
 		//Node is delegator
-		delegator(window_height,window_width,world_size-2);
+		delegator(window_height,window_width,partitions);
 	}
 	else{
 		//Node is slave
